776f2d82b008755751931ca133fd7d25a9598ffe
AlexMqs <alexandremarques73000@gmail.com>
2024-08-21 21:51:43 +0200

    Ajout de next() pour eviter les erreurs

--- backend/models/Book.js
+++ backend/models/Book.js
@@ -1,10 +1,12 @@
 const mongoose = require('mongoose');
 
 const bookSchema = new mongoose.Schema({
+    userId: { type: String, required: true },
     title: { type: String, required: true },
     author: { type: String, required: true },
-    description: { type: String, required: true },
     imageUrl: { type: String, required: true },
+    year: { type: Number, required: true },
+    genre: { type: String, required: true },
     ratings: [
         {
             userId: { type: String, required: true },
