0ea9f8d53b7f1ee5063a9169f91f9ce13cf927d6
AlexMqs <alexandremarques73000@gmail.com>
2024-08-21 16:56:47 +0200

    mise en place de toutes les routes

--- backend/routes/books.js
+++ backend/routes/books.js
@@ -1,26 +1,28 @@
 const express = require('express');
 const bookCtrl = require('../controllers/bookCtrl');
+const auth = require('auth');
 
 const router = express.Router();
 
-
+// Route pour récupérer tous les livres
 router.get('/', bookCtrl.getAllBooks);
 
+// Route pour récupérer un livre par son ID
 router.get('/:id', bookCtrl.getBookById);
 
-router.get('/bestrating', (req, res, next) => {
-});
+// Route pour récupérer les 3 livres ayant la meilleure note moyenne
+router.get('/bestrating', bookCtrl.getBestRatingBooks);
 
-router.post('/:id',(req, res, next) => {
-});
+// Route pour ajouter un nouveau livre avec une image
+router.post('/', auth, bookCtrl.addBook);
 
-router.put('/',(req, res, next) => {
-});
+// Route pour mettre à jour un livre par son ID avec ou sans image
+router.put('/:id', auth, bookCtrl.updateBook);
 
-router.delete('/:id',(req, res, next) => {
-});
+// Route pour supprimer un livre par son ID
+router.delete('/:id',auth, bookCtrl.deleteBook);
 
-router.post('/:id/rating',(req, res, next) => {
-});
+// Route pour ajouter une note à un livre pour un utilisateur donné
+router.post('/:id/rating',auth, bookCtrl.addRating);
 
 module.exports = router;
